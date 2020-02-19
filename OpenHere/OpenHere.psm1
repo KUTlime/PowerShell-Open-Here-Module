c4a1d043439c388ba6ec65a5d506f4f123ded52d
KUTlime <kutlime@gmail.com>
2020-02-19 09:32:59 +0100

    Initial implementation of main set function.

--- OpenHere/OpenHere.psm1
+++ OpenHere/OpenHere.psm1
@@ -1,15 +1,98 @@
+function Get-IconAsset
+{
+  New-Item "$env:LOCALAPPDATA\WindowsPowerShell" -ItemType Directory -Force
+  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+  $wc = New-Object System.Net.WebClient
+  $wc.DownloadFile('https://raw.githubusercontent.com/KUTlime/PowerShell-Open-Here-Module/master/assets/PowerShell.ico', "$env:LOCALAPPDATA\WindowsPowerShell\PowerShell.ico")
+}
+
 function Set-WindowsPowerShellShortcut
 {
     [CmdletBinding()]
-    param (
+    Param (
+        [Parameter(
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ValueFromRemainingArguments = $false,
+            Position = 0,
+            ParameterSetName = 'Basic')]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $RootName = "Windows PowerShell",
+        [Parameter(
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ValueFromRemainingArguments = $false,
+            Position = 1,
+            ParameterSetName = 'Basic')]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $OpenHere = "Open here",
+        [Parameter(
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ValueFromRemainingArguments = $false,
+            Position = 1,
+            ParameterSetName = 'Basic')]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $OpenHereAsAdmin = "Open here as Administrator"
     )
-    throw [System.NotImplementedException]::new()
+    $registryKeyName = 'WindowsPowerShell'
+    Get-IconAsset
+    $iconFile = Get-ChildItem -Path $env:LOCALAPPDATA\WindowsPowerShell\PowerShell.ico
+    New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -Name HKCR | Write-Verbose
+    function Set-RegistryKey
+    {
+        [CmdletBinding()]
+        param (
+            [Parameter()]
+            [String]
+            $Path
+        )
+        New-Item -Path $Path\$registryKeyName -Force -ErrorAction:Continue | Write-Verbose
+        New-ItemProperty -Path $Path\$registryKeyName -Name MUIVerb -Value $RootName -Force -ErrorAction:Continue | Write-Verbose
+        New-ItemProperty -Path $Path\$registryKeyName -Name Icon -Value $iconFile.FullName -Force -ErrorAction:Continue | Write-Verbose
+        New-ItemProperty -Path $Path\$registryKeyName -Name ExtendedSubCommandsKey -Value "Directory\ContextMenus\$registryKeyName" -Force -ErrorAction:Continue | Write-Verbose
+    }
+
+    function Set-RegistryKeyWithCommand
+    {
+        [CmdletBinding()]
+        param (
+            [Parameter()]
+            [String]
+            $Path
+        )
+        New-Item -Path $Path\$registryKeyName\shell\runas\command -Value "$env:SystemRoot\system32\WindowsPowerShell\v1.0\powershell.exe -noexit -command Set-Location '%V'" -Force -ErrorAction:Continue | Write-Verbose
+        New-Item -Path $Path\$registryKeyName\shell\openpowershell\command -Value "$env:SystemRoot\system32\WindowsPowerShell\v1.0\powershell.exe -noexit -command Set-Location '%V'" -Force -ErrorAction:Continue | Write-Verbose
+    }
+    Set-RegistryKey -Path HKCR:\LibraryFolder\background\shell
+    Set-RegistryKey -Path HKCR:\Drive\shell
+    Set-RegistryKey -Path HKCR:\Directory\shell
+    Set-RegistryKey -Path HKCR:\Directory\Background\shell
+    Set-RegistryKey -Path HKCR:\DesktopBackground\Shell
+    Set-RegistryKey -Path HKLM:\SOFTWARE\Classes\Drive\shell
+    Set-RegistryKey -Path HKLM:\SOFTWARE\Classes\Directory\shell
+    Set-RegistryKey -Path HKLM:\SOFTWARE\Classes\Directory\background\shell
+    Set-RegistryKey -Path HKLM:\SOFTWARE\Classes\DesktopBackground\Shell
+    Set-RegistryKey -Path HKLM:\SOFTWARE\Classes\LibraryFolder\background\shell
+
+    Set-RegistryKeyWithCommand -Path HKCR:\Directory\ContextMenus
+    Set-RegistryKeyWithCommand -Path HKCR:\SOFTWARE\Classes\Directory\ContextMenus
+
+    New-ItemProperty -Path HKCR:\Directory\ContextMenus\$registryKeyName\shell\runas -Name MUIVerb -Value $OpenHereAsAdmin -Force
+    New-ItemProperty -Path HKCR:\Directory\ContextMenus\$registryKeyName\shell\runas -Name Icon -Value $iconFile.FullName -Force
+    New-ItemProperty -Path HKCR:\Directory\ContextMenus\$registryKeyName\shell\runas -Name HasLUAShield -Value "" -Force
+
+    New-ItemProperty -Path HKCR:\Directory\ContextMenus\$registryKeyName\shell\openpowershell -Name MUIVerb -Value $OpenHere -Force
+    New-ItemProperty -Path HKCR:\Directory\ContextMenus\$registryKeyName\shell\openpowershell -Name Icon -Value $iconFile.FullName -Force
 }
 
 function Remove-WindowsPowerShellShortcut
 {
     [CmdletBinding()]
-    param (
+    Param (
     )
     throw [System.NotImplementedException]::new()
 }
@@ -103,6 +186,7 @@ Function Out-Icon
         $FilePath
 
     )
+    New-Item -Path ([System.IO.Path]::GetDirectoryName($FilePath)) -ItemType Directory -ErrorAction:SilentlyContinue
     [System.Reflection.Assembly]::LoadWithPartialName('System.Drawing') | Write-Verbose
-    $Icon.ToBitmap().Save($FilePath)
+    $Icon.ToBitmap().Save("$FilePath")
 }
