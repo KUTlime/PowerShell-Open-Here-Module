c5cb1c3da37a7d608ac7f4a1ae5848a88135ae3a
KUTlime <kutlime@gmail.com>
2020-07-28 08:55:08 +0200

    Update of publishing script.

--- PublishModule.ps1
+++ PublishModule.ps1
@@ -1,10 +1,16 @@
 $psGet = 'PowerShellGet'
 $nugetApiKey = $args[0]
-Write-Host "Test"
+$minimalPSGetVersion = [System.Version]'2.2.3'
 if ($nugetApiKey)
 {
     Write-Host "Api key exist."
 }
-Install-Module -Name $psGet -MinimumVersion '2.2.3'
+
 Import-Module PowerShellGet
-Get-Module | Where-Object {$_.Name -eq 'PowerShellGet'}
+$psGetModule = Get-Module | Where-Object { $_.Name -eq $psGet }
+if ($psGetModule.Version -lt $minimalPSGetVersion)
+{
+    Install-Module -Name $psGet -MinimumVersion '2.2.3'
+}
+
+Publish-Module -Name "$PSScriptRoot\OpenHere\" -NuGetApiKey $nugetApiKey
